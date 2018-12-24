ddbd7f1040eb27d22d447e0dcc873bd9e7a2e132
DelSquared <37172200+DelSquared@users.noreply.github.com>
2018-12-24 23:22:17 +0100

    Update perceptron.lua

--- perceptron.lua
+++ perceptron.lua
@@ -4,6 +4,7 @@ perceptron.inputNum = 0
 perceptron.outputNum = 0
 perceptron.weights = {}
 perceptron.rate = 0.0001
+perceptron.trainCycles=0
 
 function perceptron.Topology(topologyList)
   perceptron.inputNum = topologyList[1]
@@ -60,32 +61,45 @@ function perceptron.Eval(inputs,der)
   return outputVals
 end
 
-function perceptron.Train(inputs,outputs)
+function perceptron.Train(inputs,outputs,suppresslog)
   dt = perceptron.rate
   y=perceptron.Eval(inputs)
   dy=perceptron.Eval(inputs,true)
-  startError = perceptron.error(y,outputs)
+  --startError = perceptron.error(y,outputs)
   errorDerivative = perceptron.error(y,outputs,true)
-  for i=1,table.getn(perceptron.weights[1])do
-    for j=1,table.getn(perceptron.weights)do
-      perceptron.weights[i][j] = perceptron.weights[i][j] - errorDerivative*dt*dy[i]*inputs[j]
+  for i=1,table.getn(perceptron.weights)do
+    for j=1,table.getn(perceptron.weights[1])do
+      perceptron.weights[i][j] = perceptron.weights[i][j] - errorDerivative*dt*dy[j]*inputs[i]
     end
   end
   finalError = perceptron.error(perceptron.Eval(inputs),outputs)
-  print("Starting Error: "..startError..", Ending Error: "..finalError)
+  perceptron.trainCycles = perceptron.trainCycles+1
+  if suppresslog==nil then
+    print("Starting Error: "..startError..", Ending Error: "..finalError)
+  elseif suppresslog=="sparse" and perceptron.trainCycles%100000==0 then
+    print("Error: "..finalError)
+  end
+  
 end
 
 -------------------------------------------------------------------
 -------------------------------------------------------------------
+----------------------Testing Section Below------------------------
+-------------------------------------------------------------------
 -------------------------------------------------------------------
 
-perceptron.Topology({3,3})
+perceptron.Topology({3,2})
 
-outputs = perceptron.Eval({1,1,1})
+outputs = perceptron.Eval({1,0,1})
 for i=1,table.getn(outputs) do
   print(outputs[i])
 end
 
-for i=1,100000 do
-  perceptron.Train({1,0,1},{0,1,0})
+for i=1,1000000 do
+  perceptron.Train({1,0,1},{1,0},"sparse")
+end
+
+outputs = perceptron.Eval({1,0,1})
+for i=1,table.getn(outputs) do
+  print(outputs[i])
 end
